a3e952972f5820370ea3c17f7044c8bc506cb2c9
George Lifchits <george.lifchits@gmail.com>
2013-07-05 03:55:00 -0400

    string slide representation kinda working

--- src/traverse_xml.py
+++ src/traverse_xml.py
@@ -10,12 +10,66 @@ class Slide:
     '''
     A slide. Given the .pptx XML for a specific PowerPoint slide, this class creates a nice nested
     dictionary representation of the bare minimum content.
+
+    self.slide will be parsed and the structure looks like this:
+
+    [{ id: int
+       paragraphs: [{ id: int
+                      words: [{ id: int
+                                text: ""
+                                italic: int
+                                baseline: int }]
+                    }]
+    }]
+
+
+
     '''
+    INDENT = '  '
 
     def __init__( self, xml_string ):
         self.xml = BeautifulSoup( xml_string )
+        self.slide = self._parse_slide()
 
-    def get_attrs( self, parent, tag ):
+    def __str__( self ):
+        def wrap( string, tag ):
+            if tag.startswith( '<' ) and tag.endswith( '>' ):
+                open = tag
+                close = tag[:-1] + '/>'
+            else:
+                open, close = tag, tag
+
+            return open + string + close
+
+        def delimiters( string ):
+            pass
+
+        s = ""
+        by_id = lambda x: x['id']
+        for part in sorted( self.slide, key = by_id ):
+            para = ""
+            if 'type' in part and part['type'] == 'title':
+                para += "# "
+
+            for paragraph in sorted( part['paragraphs'], key = by_id ):
+                sentence = ""
+                if 'indent' in paragraph:
+                    sentence += self.INDENT * paragraph['indent'] + ' '
+
+                for word in sorted( paragraph['words'], key = by_id ):
+                    this_word = word['text']
+                    if 'italic' in word:
+                        this_word = wrap( this_word, "<i>" )
+                    if 'baseline' in word:
+                        this_word = wrap( this_word, "<sup>" )
+                    delimiters( this_word )  # TODO: Markdown characters that need to be delimited
+                    sentence = sentence.strip() + this_word + ' '
+                para += sentence + '\n'
+            s += para + '\n'
+        return s
+
+
+    def _get_attrs( self, parent, tag ):
         attrs = []
         a = parent.find( tag )
         if a is None:
@@ -42,7 +96,7 @@ class Slide:
 
         return attrs
 
-    def get_slide( self ):
+    def _parse_slide( self ):
         '''
         <p:sp>      a part, a logically contiguous piece. eg, title, content, slide number
             <p:ph>    attribute tag
@@ -61,7 +115,7 @@ class Slide:
         for sp_id, sp in enumerate( self.xml.find_all( 'p:sp' ) ):
             part = {'id':sp_id, 'paragraphs':[]}
 
-            for key, value in self.get_attrs( sp, 'p:ph' ):
+            for key, value in self._get_attrs( sp, 'p:ph' ):
                 part[key] = value
 
             txt = sp.find( 'p:txbody' )
@@ -71,14 +125,14 @@ class Slide:
 
                 par = {'id': p_id + 1, 'words':[]}
 
-                for key, value in self.get_attrs( p, "a:ppr" ):
+                for key, value in self._get_attrs( p, "a:ppr" ):
                     par[key] = value
 
                 for r_id, r in enumerate( p.find_all( 'a:r' ) ):
                     word = {'id':r_id,
                             'text': r.find( 'a:t' ).text.strip()}  # this is the text portion!
 
-                    for key, value in self.get_attrs( r, "a:rpr" ):
+                    for key, value in self._get_attrs( r, "a:rpr" ):
                         word[key] = value
 
                     par['words'].append( word )
@@ -87,9 +141,9 @@ class Slide:
 
             parts.append( part )
 
-        pprint.pprint( parts )
+        return parts
 
 if __name__ == "__main__":
     with open( "sample.xml" ) as xml_file:
         s = Slide( xml_file.read() )
-        s.get_slide()
+        print s
